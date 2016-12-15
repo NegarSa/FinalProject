93d0ef9bf8b8301ef2c9ffc88c4251d4d8c7e6a2
sepantou <r.sepantou@gmail.com>
2016-12-16 00:38:16 +0330

    next version

--- trial_editor.cpp
+++ trial_editor.cpp
@@ -67,9 +67,8 @@ int main()
 		case CTRLS:
 			printtofile(start);
 		case CTRLF:
-		{
 			findstring(start);
-		}
+			break;
 		case 0:
 		case KEYCONST:
 			character = getch();
@@ -108,30 +107,32 @@ int main()
 				toright(&position);
 				Delete(&start, &position);
 				print(start, position);
-			}
-		case ALTF4:
-		{
-			char choice;
-			openexe("getuserchoice.exe", "Do_you_want_to_exit_?_press_'y'_for_yes_and_'n'_for_no!!!");
-			FILE *fptr;
-			fptr = fopen("userchoice.txt", "r");
-			choice = fgetc(fptr);
-			fclose(fptr);
-			if (choice == 'n' || choice == 'N')
-				break;
-			if (prevchoice == CTRLS)
+
+			case ALTF4:
 			{
-				openexe("printmassage.exe", "going_to_exit");
-				return 0;
+				char choice;
+				openexe("getuserchoice.exe", "Do_you_want_to_exit_?_press_'y'_for_yes_and_'n'_for_no!!!");
+				FILE *fptr;
+				fptr = fopen("userchoice.txt", "r");
+				choice = fgetc(fptr);
+				fclose(fptr);
+				if (choice == 'n' || choice == 'N')
+					break;
+				if (prevchoice == CTRLS)
+				{
+					openexe("printmassage.exe", "going_to_exit");
+					return 0;
+				}
+				else
+				{
+					printtofile(start);
+					openexe("printmassage.exe", "going_to_exit");
+					return 0;
+				}
 			}
-			else
-			{
-				printtofile(start);
-				openexe("printmassage.exe", "going_to_exit");
-				return 0;
+		
 			}
-		}
-		break;
+			break;
 		default:
 			insert(&start, &position, character);
 			print(start, position);
@@ -537,10 +538,10 @@ void findstring(nodeptr start)
 	for (i = 0; cur != NULL; i++)
 	{
 		if (i == *(num + j))
-			printf("\x1B[32m");
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 1760 | 011);
 		if (i == (*(num + j))+phraselen)
 		{
-			printf("\x1B[37m");
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
 			j++;
 		}
 		switch (cur->data) {
