867a2f448f7ad39bb74fcb3746fd0d8e3a79ad0a
Negar Sakhaei <negar.sakhaei@yahoo.com>
2016-12-29 00:52:34 +0330

    Update mainproject.cpp

--- source files/mainproject.cpp
+++ source files/mainproject.cpp
@@ -1,14 +1,16 @@
 #include"functions.h"
+
 int main()
 {
 	char *command;
 	char **arguments;
+	int argnum;
 	while (1) {
 		command = getcommand();
-		arguments = parscommand(command);
-		runcommand(arguments);
+		arguments = parscommand(command, &argnum);
+		runcommand(arguments, argnum);
+		free(command);
+		free(arguments);
 	}
-	free(command);
-	free(arguments);
 	return 0;
 }
