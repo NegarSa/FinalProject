0a3e1014b8f1502ec5d3514c027e7943d112fbf2
Negar Sakhaei <negar.sakhaei@yahoo.com>
2016-12-29 00:48:56 +0330

    Update functions.cpp
    we needed the number of the arguments of every command, so i added it. (it's called argnum)

--- source files/functions.cpp
+++ source files/functions.cpp
@@ -30,7 +30,7 @@ char *getcommand()
 
 	}
 }
-char **parscommand(char *command) {
+char **parscommand(char *command, int * argnum) {
 	char *argument;
 	int size = 5;
 	char **arguments = (char **)malloc(size*(sizeof(char *)));
@@ -46,11 +46,12 @@ char **parscommand(char *command) {
 		}
 	}
 	arguments[position] = NULL;
+	*argnum = position;
 	return arguments;
 }
 int runcommand(char **arguments)
 {
-	int(*functionpointers[]) (char **) = {
+	int(*functionpointers[]) (char **, int) = {
 		&su,&passwd,&createuser,&cal,&time,&clear,
 		&exit,&history,&cd,&pwd,&mkdir,&cat,
 		&rm,&cp,&mv,&ls,&exif,&myeditor,&wc,&diff,&help,&game, };
@@ -58,108 +59,108 @@ int runcommand(char **arguments)
 		return 0;
 	for (int m = 0; m < COMMNUMBER; m++)
 		if (!strcmp(arguments[0], functionsname[m]))
-			return(*functionpointers[m])(arguments);
+			return(*functionpointers[m])(arguments, argnum);
 	puts("the command is not valid!!!");
 	return 0;
 }
 
 //functions to run commands
-int su(char **arguments)
+int su(char **arguments, int argnum)
 {
 	puts("su");
 	return 0;
 }
-int passwd(char **arguments)
+int passwd(char **arguments, int argnum)
 {
 	puts("passwd");
 	return 0;
 }
-int createuser(char **arguments)
+int createuser(char **arguments, int argnum)
 {
 	puts("createuser");
 	return 0;
 }
-int cal(char **arguments)
+int cal(char **arguments, int argnum)
 {
 	puts("cal");
 	return 0;
 }
-int time(char **arguments)
+int time(char **arguments, int argnum)
 {
 	puts("time");
 	return 0;
 }
-int clear(char **arguments)
+int clear(char **arguments, int argnum)
 {
 	puts("clear");
 	return 0;
 }
-int exit(char **arguments)
+int exit(char **arguments, int argnum)
 {
 	puts("exit");
 	return 0;
 }
-int history(char **arguments)
+int history(char **arguments, int argnum)
 {
 	puts("history");
 	return 0;
 }
-int cd(char **arguments)
+int cd(char **arguments, int argnum)
 {
 	puts("cd");
 	return 0;
 }
-int pwd(char **arguments)
+int pwd(char **arguments, int argnum)
 {
 	puts("pwd");
 	return 0;
 }
-int mkdir(char **arguments)
+int mkdir(char **arguments, int argnum)
 {
 	puts("mkdir");
 	return 0;
 }
-int cat(char **arguments)
+int cat(char **arguments, int argnum)
 {
 	puts("cat");
 	return 0;
 }
-int rm(char **arguments)
+int rm(char **arguments, int argnum)
 {
 	puts("rm");
 	return 0;
 }
-int cp(char **arguments)
+int cp(char **arguments, int argnum)
 {
 	puts("cp");
 	return 0;
 }
-int mv(char **arguments)
+int mv(char **arguments, int argnum)
 {
 	puts("mv");
 	return 0;
 }
-int ls(char **arguments)
+int ls(char **arguments, int argnum)
 {
 	puts("ls");
 	return 0;
 }
-int exif(char **arguments)
+int exif(char **arguments, int argnum)
 {
 	puts("exif");
 	return 0;
 }
-int wc(char **arguments)
+int wc(char **arguments, int argnum)
 {
 	puts("wc");
 	return 0;
 }
-int diff(char **arguments)
+int diff(char **arguments, int argnum)
 {
 	puts("diff");
 	return 0;
 }
-int help(char **arguments)
+int help(char **arguments, int argnum)
 {
 	puts("help");
 	return 0;
